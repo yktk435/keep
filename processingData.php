c2d2a69fad84a7d5836284b5b5f8ef3fb1672e8a
yktk435 <yokotsuka435@gmail.com>
2020-06-22 02:10:05 +0900

    color_idの制約をintからvarcharに変更

--- processingData.php
+++ processingData.php
@@ -5,17 +5,31 @@ try {
     $db = getDb();
     if ($_POST['update']) {
         $data=json_decode($_POST['update'], true);//第2引数をtrueにしないと$dataが連想配列にならない
+        check($data);
         update($db, $data);
+        $response['data']='update';
+        print json_encode($response);
     } elseif ($_POST['remove']) {
         $data=json_decode($_POST['remove'], true);//第2引数をtrueにしないと$dataが連想配列にならない
+        check($data);
         remove($db, $data);
+        $response['data']='remove';
+        print json_encode($response);
     } elseif ($_POST['create']) {
         $data=json_decode($_POST['create'], true);//第2引数をtrueにしないと$dataが連想配列にならない
+        check($data);
         create($db, $data);
-        print $db->lastInsertId();
+        $response['data']=$db->lastInsertId();
+        print json_encode($response);
     }
 } catch (PDOException $e) {
-    print "エラーメッセージ：{$e->getMessage()}";
+    http_response_code(404);
+    $response['data']=$e->getMessage();
+    print json_encode($response);
+} catch (Exception $e) {
+    http_response_code(404);  
+    $response['data']=$e->getMessage();
+    print json_encode($response);
 } finally {
     $_POST=[];
 }
@@ -30,7 +44,7 @@ function update($db, $data)
     $stt->bindValue(':datetime', $data['datetime']);
     $stt->bindValue(':contents', $data['contents']);
     $stt->bindValue(':label', $data['label']);
-    $stt->bindValue(':color_id', (int)$data['color_id']);
+    $stt->bindValue(':color_id', $data['color_id']);
     $stt->bindValue(':user_id', (int)$data['user_id']);
     $stt->execute();
 }
@@ -38,7 +52,7 @@ function update($db, $data)
 function remove($db, $data)
 {
     $stt = $db->prepare('DELETE FROM memo WHERE id=:id');
-  
+    
     $stt->bindValue(':id', $data['id']);
     $stt->execute();
 }
@@ -51,7 +65,19 @@ function create($db, $data)
     $stt->bindValue(':datetime', $data['datetime']);
     $stt->bindValue(':contents', $data['contents']);
     $stt->bindValue(':label', $data['datetime']);
-    $stt->bindValue(':color_id', (int)$data['color_id']);
+    $stt->bindValue(':color_id', $data['color_id']);
     $stt->bindValue(':user_id', (int)$data['user_id']);
     $stt->execute();
+}
+
+function check($data)
+{
+    if ($data['title']=='' && $data['contents']=='') {
+        throw new Exception('タイトルと内容がどちらも空です。');
+    }
+    if($_POST['remove']){
+      if($data['id']==''){
+        throw new Exception('メモIDが空です。');
+      }
+    }
 }
