585e85cef375c002ae5ba3dfbaede02a86a46c1f
ohshimo <ohshimo@gmail.com>
2020-07-03 19:32:30 +0900

    create sample.js

---
+++ js/sample.js
@@ -0,0 +1,46 @@
+'use strict';
+
+class Memo {
+    constructor(title, content){
+        this.id = 0;
+        this.title = title;
+        this.content = content;
+        this.datetime = new Date();
+    }
+    
+    createLiTag(){
+        let li = document.createElement("li");
+        li.textContent = this.content;
+        li.addEventListener('click' function(){
+            console.log(this);
+        }.bind(this));
+    }
+    
+}
+
+for(let i = 0; i<10; i++){
+    
+}
+
+
+class ApiManager {
+    constructor(){
+        
+    }
+    
+    fetchAllData(){
+        
+    }
+    
+    createMemo(Memo memo){
+        
+    }
+    
+    deleteMemo(Memo memo){
+        
+    }
+    
+    updateMemo(Memo memo){
+        
+    }
+}
