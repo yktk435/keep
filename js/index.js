9643b784fe32a2455e96553fa531df6dbd37c950
yktk435 <yokotsuka435@gmail.com>
2020-07-04 17:53:16 +0900

    後で消す

--- js/index.js
+++ js/index.js
@@ -1,48 +1,126 @@
 'use strict';
 
 class Memo {
-    constructor(title, content){
-        this.id = 0;
-        this.title = title;
-        this.content = content;
-        this.datetime = new Date();
-    }
-    
-    createLiTag(){
-        let li = document.createElement("li");
-        li.textContent = this.content;
-        li.addEventListener('click' function(){
-            console.log(this);
-        }.bind(this));
-    }
-    
-}
+  constructor(title, contents,label={}) {
+    this.id = 0;
+    this.title = title;
+    this.contents = contents;
+    this.datetime = new Date();
+    this.label=label;
+  }
 
-for(let i = 0; i<10; i++){
-    let memo = new Memo();
-    memo.id = i;
-    document.getElementById("contents").appendChild(memo.createLiTag());
+  createDivTag() {
+    let div = document.createElement("div");
+    div.id='id_'+this.id
+    div.textContent = this.contents;
+    div.className="memo share";
+    
+    div.innerHTML = `
+    <div contenteditable="true" class="textArea"  onkeyup="keyUp(this)">${this.title}</div>
+    <div contenteditable="true" class="textArea"  onkeyup="keyUp(this)">${this.content}</div>
+    <div id="id_${this.id}" class="label-area">`;
+    
+    Object.keys(this.label).forEach((key)=>{
+    div.innerHTML += `<div label_id="${key}" class="label">${this.label[key]}</div>`;  
+    })
+    div.innerHTML += `
+    </div>
+    <div class="">
+      <button type="button" name="button" onclick=removeObj(this)>削除_${this.id}</button>
+    </div>
+    <div class="other_menu">
+      <ul class="gnav">
+        <li>カラー 
+          <ul id="id_${this.id}">
+            <li id="def" onclick=changeColor(this)>def</li>
+            <li id="red" onclick=changeColor(this)>赤</li>
+            <li id="blue" onclick=changeColor(this)>青</li>
+            <li id="yellow" onclick=changeColor(this)>黄</li>
+          </ul>
+        </li>
+        <li id="id_${this.id}}" class="label-menu">ラベル
+        </li>
+      </ul>
+    </div>
+    `;
+    
+    div.addEventListener('DOMFocusOut', function(e) {
+      console.log(e);
+      console.log(this);
+    }.bind(this));
+    return div;
+  }
+
+}
+window.onload = function() {
+  // for (let i = 0; i < 10; i++) {
+  //   let memo = new Memo();
+  //   memo.id = i;
+  //   document.querySelector(".memo_area").appendChild(memo.createDivTag());
+  // }
+
+  let g = new ApiManager();
+  let memoObj = {}
+  g.fetchAllData().then(function(json) {
+    console.log(json)
+    let res=JSON.parse(json);
+    let resMemo = res.memo
+    let resLabel= res.label
+    let resLabelMiddle= res.labelMiddle
+    console.log(res)
+    
+    resMemo.forEach(e => {
+      Object.keys(res.labelMiddle).forEach(()=>{
+        
+      })
+      let memo=new Memo(e.title, e.contents,resLabel[e.id]);
+      console.log(memo)
+        memo.id=e.id;
+        memo.datetime=e.datetime;
+        document.querySelector(".memo_area").appendChild(memo.createDivTag());
+      memoObj = Object.assign(memoObj, {
+        [e.id]:memo 
+      })
+    })
+    console.log(memoObj)
+  })
+
 }
 
 
 class ApiManager {
-    constructor(){
-        
+  constructor() {
+    this.url = 'http://localhost:8888/keep/processingData.php';
+    this.data = {
+      test: 'ok'
     }
-    
-    fetchAllData(){
-        
-    }
-    
-    createMemo(Memo memo){
-        
-    }
-    
-    deleteMemo(Memo memo){
-        
-    }
-    
-    updateMemo(Memo memo){
-        
-    }
+  }
+
+  fetchAllData() {
+    // 既定のオプションには * が付いています
+    return fetch(this.url, {
+        method: "POST", // *GET, POST, PUT, DELETE, etc.
+        headers: {
+          // "Content-Type": "application/json; charset=utf-8",
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: '&get=' + JSON.stringify(this.data), // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+      })
+      .then(function(response) {
+        return response.text()
+      });
+
+  }
+
+  // createMemo(Memo memo) {
+  // 
+  // }
+  // 
+  // deleteMemo(Memo memo) {
+  // 
+  // }
+  // 
+  // updateMemo(Memo memo) {
+  // 
+  // }
 }
--- processingData.php
+++ processingData.php
@@ -21,15 +21,41 @@ try {
     } elseif ($_POST['createLabel']) {
         $data=json_decode($_POST['createLabel'], true);//第2引数をtrueにしないと$dataが連想配列にならない
         if ($data['label_name']) {
-            $response['labelId']=createLabel($db, $data);;
+            $response['labelId']=createLabel($db, $data);
+            ;
         }
+    } elseif ($_POST['test']) {
+        $data=json_decode($_POST['addLabel'], true);//第2引数をtrueにしないと$dataが連想配列にならない
+        //print_r($data);
+        $response['data']='aaa';
     } elseif ($_POST['removeLabel']) {
-    }elseif ($_POST['addLabel']) {
-      $data=json_decode($_POST['addLabel'], true);//第2引数をtrueにしないと$dataが連想配列にならない
-      addLabel($db,$data);  
-    }elseif ($_POST['removeLabelLink']) {
-      $data=json_decode($_POST['removeLabelLink'], true);//第2引数をtrueにしないと$dataが連想配列にならない
-      removeLabelLink($db,$data);  
+    } elseif ($_POST['addLabel']) {
+        $data=json_decode($_POST['addLabel'], true);//第2引数をtrueにしないと$dataが連想配列にならない
+        addLabel($db, $data);
+    } elseif ($_POST['removeLabelLink']) {
+        $data=json_decode($_POST['removeLabelLink'], true);//第2引数をtrueにしないと$dataが連想配列にならない
+        removeLabelLink($db, $data);
+    }elseif ($_POST['get']) {
+      $stt = $db->prepare('SELECT * FROM memo ORDER BY id DESC');
+      $stt->execute();
+      while ($row=$stt->fetch(PDO::FETCH_ASSOC)) {
+          $res[]=$row;
+      }
+      $stt = $db->prepare('SELECT memo_id,label_id FROM label_middle');
+      $stt->execute();
+      $memoIdlabelIdRes=$row=$stt->fetchAll(PDO::FETCH_COLUMN | PDO::FETCH_GROUP);
+      
+      $stt = $db->prepare('SELECT * FROM label');
+      $stt->execute();
+      while ($row=$stt->fetchAll(PDO::FETCH_COLUMN | PDO::FETCH_UNIQUE)) {
+          $labelData=$row;
+      }
+      
+      $response['memo']=$res;
+      $response['labelMiddle']=$memoIdlabelIdRes;
+      $response['label']=$labelData;
+    }else{
+      print 'NG';
     }
 } catch (PDOException $e) {
     http_response_code(404);
@@ -63,7 +89,7 @@ function remove($db, $data)
 {
     $stt = $db->prepare('DELETE FROM memo WHERE id=:id');
     $stt->bindValue(':id', $data['id']);
-        $stt->execute();
+    $stt->execute();
     $stt = $db->prepare('DELETE FROM label_middle WHERE memo_id=:id');
     $stt->bindValue(':id', $data['id']);
     $stt->execute();
@@ -96,19 +122,19 @@ function createLabel($db, $data)
     $stt->execute();
     return $lastId;
 }
-function addLabel($db,$data){
-  $stt = $db->prepare('INSERT INTO label_middle (label_id,memo_id) VALUES(:label_id,:memo_id)');
-  $stt->bindValue(':label_id', $data['label_id']);
-  $stt->bindValue(':memo_id', $data['memo_id']);
-  $stt->execute();
-  
+function addLabel($db, $data)
+{
+    $stt = $db->prepare('INSERT INTO label_middle (label_id,memo_id) VALUES(:label_id,:memo_id)');
+    $stt->bindValue(':label_id', $data['label_id']);
+    $stt->bindValue(':memo_id', $data['memo_id']);
+    $stt->execute();
 }
-function removeLabelLink($db,$data){
-  $stt = $db->prepare(' DELETE FROM label_middle WHERE label_id=:label_id AND memo_id=:memo_id');
-  $stt->bindValue(':label_id', $data['label_id']);
-  $stt->bindValue(':memo_id', $data['memo_id']);
-  $stt->execute();
-
+function removeLabelLink($db, $data)
+{
+    $stt = $db->prepare(' DELETE FROM label_middle WHERE label_id=:label_id AND memo_id=:memo_id');
+    $stt->bindValue(':label_id', $data['label_id']);
+    $stt->bindValue(':memo_id', $data['memo_id']);
+    $stt->execute();
 }
 function check($data)
 {
